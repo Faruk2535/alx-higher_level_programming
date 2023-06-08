4ec52a8cb8b404bfbb0f4f46b5b0eaf8fc1eeb3f
faruk2535 <farukonibudo@gmail.com>
2023-06-08 06:42:16 -0700

    new

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 
 """import add function add_0"""
-import add_0
+from add_0 import add
 
-add_0.add(1,2)
+a = 1
+b = 2
+print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
