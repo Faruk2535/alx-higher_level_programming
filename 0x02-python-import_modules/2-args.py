9757ad24eeae65db4938ef18999d867c696c008b
faruk2535 <farukonibudo@gmail.com>
2023-06-08 14:14:40 -0700

    new

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    arg = sys.argv
+    if len(sys.argv) == 1:
+        print("0 argument.")
+    elif len(sys.argv) > 2:
+       print("{} argument:".format(len(sys.argv)-1))
+       i = 1
+       while i < (len(sys.argv)):
+           print("{}: {}".format(i, sys.argv[i]))
+           i = i + 1
