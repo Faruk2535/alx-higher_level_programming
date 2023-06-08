c84f8f5305a5d58b0f1c57f331dd23f67404f530
faruk2535 <farukonibudo@gmail.com>
2023-06-08 07:27:52 -0700

    new

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 """import calculator for add, sub, mull and div of numbers"""
 
 from calculator_1 import add, sub, mul, div
-if __name__== "__main__":
+if __name__ == "__main__":
     a = 10
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
